cbfbe430a0d605d426178ef81864237a464b9f6f
denis <ar.denis.by@gmail.com>
2025-10-21 11:33:47 +0300

    lab3

---
+++ lab3/lab3_client/lab3_client/lab3_client.cpp
@@ -0,0 +1,99 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include "cursor_visibility.h"
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <string>
+#include <thread>
+#include <chrono>
+
+static void ClearAddrAndSockInfo(SOCKET socketName, ADDRINFO* pResult, bool ex = 1) {
+	closesocket(socketName);
+	freeaddrinfo(pResult);
+	WSACleanup();
+	exit(ex);
+}
+
+int main(int argc, char* argv[]) {
+	setlocale(0, "ru");
+
+	WSADATA wsadata;
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+		std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
+		exit(1);
+	}
+
+	ADDRINFO hints;
+	ADDRINFO* addrResult = NULL;
+	ZeroMemory(&hints, sizeof(hints));
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	if (getaddrinfo("localhost", "1111", &hints, &addrResult) != 0) {
+		std::cerr << "getaddrinfo failed with error: " << gai_strerrorA(WSAGetLastError()) << std::endl;
+		WSACleanup();
+		exit(1);
+	}
+
+	SOCKET ClientSocket = INVALID_SOCKET;
+	ClientSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+	if (ClientSocket == INVALID_SOCKET) {
+		std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		exit(1);
+	}
+
+	if (connect(ClientSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen) != 0) {
+		std::cerr << "Connect failed with error: " << WSAGetLastError() << std::endl;
+		ClientSocket = INVALID_SOCKET;
+		ClearAddrAndSockInfo(ClientSocket, addrResult);
+	}
+
+	hideCursor();
+	std::cout << "Подключение к серверу устанавливается";
+	for (size_t i = 0; i < 3; i++) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(700));
+		std::cout << ".";
+	}
+	showCursor();
+
+	std::string str;
+	std::cout << "\nВведите строку, нажмите 'Enter', чтобы отправить:\n";
+	getline(std::cin, str);
+
+	if (send(ClientSocket, str.c_str(), (int)str.length(), 0) == SOCKET_ERROR) {
+		std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
+	}
+
+	if (shutdown(ClientSocket, SD_SEND) == SOCKET_ERROR) {
+		std::cerr << "Shutdown failed with error: " << WSAGetLastError() << std::endl;
+		ClearAddrAndSockInfo(ClientSocket, addrResult);
+	}
+
+	char recvBuffer[512];
+	int result;
+	std::string receivedData;
+
+	do {
+		ZeroMemory(recvBuffer, sizeof(recvBuffer));
+
+		hideCursor();
+		result = recv(ClientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
+		if (result > 0) {
+			receivedData.append(recvBuffer, result);
+		}
+
+	} while (result > 0);
+
+	if (!receivedData.empty()) {
+		std::cout << receivedData << std::endl;
+	}
+
+	ClearAddrAndSockInfo(ClientSocket, addrResult, 0);
+}
---
+++ lab3/lab3_server/lab3_server/lab3_server.cpp
@@ -0,0 +1,140 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <string>
+#include <algorithm>
+#include <thread>
+#include <vector>
+
+static void ClearAddrAndSockInfo(SOCKET socketName, ADDRINFO* pResult, bool ex = 1) {
+    if (socketName != INVALID_SOCKET) {
+        closesocket(socketName);
+    }
+    if (pResult != NULL) {
+        freeaddrinfo(pResult);
+    }
+    WSACleanup();
+    if (ex) exit(ex);
+}
+
+static std::string processString(const std::string& input) {
+    std::string result = input;
+
+    if (result.length() % 3 == 0) {
+        for (int i = 0; i < result.length(); i++) {
+            if (isdigit(result[i])) {
+                int digit = result[i] - '0';
+                if (digit != 0 && digit % 3 == 0) {
+                    result.erase(i, 1);
+                    i--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+static void handleClient(SOCKET СlientSocket) {
+    std::cout << "Обработка клиента в потоке " << std::this_thread::get_id() << std::endl;
+
+    char recvBuffer[512];
+    int result;
+    std::string receivedData;
+
+    do {
+        ZeroMemory(recvBuffer, sizeof(recvBuffer));
+
+        result = recv(СlientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
+        if (result > 0) {
+            receivedData.append(recvBuffer, result);
+        }
+
+    } while (result > 0);
+
+    if (!receivedData.empty()) {
+        std::cout << "Получено от клиента: " << receivedData << std::endl;
+        std::string processedString = processString(receivedData);
+        std::cout << "Обработанная строка: " << processedString << std::endl;
+
+        if (send(СlientSocket, processedString.c_str(), (int)processedString.length(), 0) == SOCKET_ERROR) {
+            std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
+        }
+    }
+
+    if (shutdown(СlientSocket, SD_SEND) == SOCKET_ERROR) {
+        std::cerr << "Shutdown failed with error: " << WSAGetLastError() << std::endl;
+    }
+    closesocket(СlientSocket);
+
+    std::cout << "Клиент отключен, поток завершен" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(0, "ru");
+
+    WSADATA wsadata;
+    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+        std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
+        exit(1);
+    }
+
+    ADDRINFO hints;
+    ADDRINFO* addrResult = NULL;
+    ZeroMemory(&hints, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    if (getaddrinfo(NULL, "1111", &hints, &addrResult) != 0) {
+        std::cerr << "getaddrinfo failed with error: " << gai_strerrorA(WSAGetLastError()) << std::endl;
+        WSACleanup();
+        exit(1);
+    }
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    ListenSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
+        freeaddrinfo(addrResult);
+        WSACleanup();    
+        exit(1);
+    }
+
+    if (bind(ListenSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen) == SOCKET_ERROR) {
+        std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
+        ClearAddrAndSockInfo(ListenSocket, addrResult);
+    }
+
+    int availableConnections = SOMAXCONN;
+
+    if (listen(ListenSocket, availableConnections) == SOCKET_ERROR) {
+        std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
+        ClearAddrAndSockInfo(ListenSocket, addrResult);
+    }
+
+    std::cout << "Сервер запущен и ожидает подключения..." << std::endl;
+
+    std::vector<std::thread> clientThreads;
+    while (true) {
+        SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
+        if (ClientSocket == INVALID_SOCKET) {
+            std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
+            continue;
+        }
+
+        std::cout << "Новый клиент подключен!" << std::endl;
+
+        clientThreads.emplace_back(handleClient, ClientSocket);
+
+        clientThreads.back().detach();
+    }
+
+    closesocket(ListenSocket);
+    ClearAddrAndSockInfo(INVALID_SOCKET, addrResult, 0);
+    return 0;
+}
